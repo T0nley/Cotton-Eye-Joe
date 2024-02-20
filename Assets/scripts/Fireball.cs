88cea3f9c246db91b5aa4e9a7a3f2f535ffb119c
T0nley <dentarasov-1124093053@mail.ru>
2024-02-20 15:45:41 +0300

    20.02.2024

---
+++ Assets/scripts/Fireball.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public float speed;
+    public float lifetime;
+
+    // Start is called before the first frame pdate
+    private void FixedUpdate()
+    {
+        
+        MoveFixedUpdate();
+    }
+   
+
+    // Update is called once per frame
+    private void MoveFixedUpdate()
+    {
+            transform.position += transform.forward * speed * Time.fixedDeltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        DestroyFireball();
+    }
+
+    private void DestroyFireball()
+    {
+        Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        Invoke("DestroyFireball", lifetime);
+    }
+
+
+
+}
