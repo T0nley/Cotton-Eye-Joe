04cf8c584c5a06bb88f10fab8c08da7e6fe75820
T0nley <dentarasov-1124093053@mail.ru>
2024-03-23 21:51:13 +0300

    23.03.2024 21:50

--- Assets/scripts/EnemyHealth.cs
+++ Assets/scripts/EnemyHealth.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour
 
 {
+    public PlayerProgress playerProgress;
 
     public float value = 100;
     
@@ -13,7 +14,7 @@ public class EnemyHealth : MonoBehaviour
     // Update is called once per frame
     private void Start()
     {
-        
+        playerProgress = FindObjectOfType<PlayerProgress>();  
     }
 
     private void Update()
@@ -22,11 +23,13 @@ public class EnemyHealth : MonoBehaviour
     }
     public void DealDamage(float damage)
     {
+        playerProgress.AddExperience(damage);
         value -= damage;
+        
         if (value <= 0)
         {
             Destroy(gameObject);
-            
+           
         }     
     }
 
---
+++ Assets/scripts/PlayerProgress.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerProgress : MonoBehaviour
+{
+    public List<PlayerProgressLevel> levels;
+    //Для этого, как обычно, понадобится ссылка на соответствующий объект интерфейса.
+    //В нашем случае — на компонент TextMeshProUGUI. Добавь публичное поле и назови его levelValueTMP.
+    public TextMeshProUGUI levelValueTMP;
+    //Изменить эту координату нужно внутри метода DrawUI, поэтому нам понадобится ссылка на компонент RectTransform жёлтого прямоугольника
+    //(шкалы опыта). Для этого создадим публичное поле и назовём его experienceValueRectTransform.
+    public RectTransform experienceValueRectTransform;
+    //Заведём приватное поле типа int и назовём его _levelValue.
+    //Это поле будет отвечать за значение уровня игрока.
+    //На старте игры уровень равен одному.
+    private int _levelValue = 1;
+    //Заведи два приватных поля типа float и назови их _experienceCurrentValue (для текущих очков опыта)
+    //и _experienceTargetValue (для очков, необходимых, чтобы перейти на следующий уровень).
+    //В значениях по умолчанию укажи 0 для первого и 100 для второго.
+    private float _experienceCurrentValue = 0;
+    private float _experienceTagetValue = 100;
+
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        SetLevel(_levelValue);
+        DrawUI();
+    }
+    public void AddExperience(float value)
+    {
+        _experienceCurrentValue += value;
+        if(_experienceCurrentValue >= _experienceTagetValue)
+        {            
+            _experienceCurrentValue -= _experienceTagetValue;
+            SetLevel(_levelValue += 1);
+        }
+        DrawUI();
+    }
+
+    private void SetLevel(int value)
+    {
+        _levelValue = value;
+
+        _experienceTagetValue = levels[_levelValue - 1].experieneceForTheNextLevel;
+    }
+    // Update is called once per frame
+    private void DrawUI()
+    {
+        experienceValueRectTransform.anchorMax = new Vector2(_experienceCurrentValue / _experienceTagetValue, 1);
+        levelValueTMP.text = _levelValue.ToString();
+    }
+}
